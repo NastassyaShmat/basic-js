d0634dc5d87a07b8fdb64e4f4b79c38c8438a7a4
Nastassia Shmat <yatsan.shmat@gmail.com>
2021-03-17 20:33:52 +0300

    feat: added new functionality

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard) {
+  let str = String(backyard).split(',');
+  let arr = [];
+for (let i = 0; i < str.length; i++){
+    if (str[i] == '^^'){
+        arr.push(i);
+    }
+}
+return arr.length;
+
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(a) {
+  let k = a.map(function name(number) {
+    
+    if (a.length === 0){
+      return number;
+    }
+    if (number === '--double-next') {
+      m = a.indexOf('--double-next');
+      if (m === a.length-1) {
+        number = 'delete';
+       return number;
+      }
+   number = a[m+1];
+      return number;
+   }
+    if (number === '--double-prev') {
+      m = a.indexOf('--double-prev');
+      if (m === 0) {
+        number = 'delete';
+       return number;
+      }  
+   number = a[m-1];
+   
+      return number;
+     }
+    if (number === '--discard-next') {
+     m = a.indexOf('--discard-next');
+     if (m === a.length-1) {
+      number = 'delete';
+     return number;
+    }
+      a[m+1]= 'delete';
+      number = 'delete';
+       return number;
+   }
+    if (number === '--discard-prev') {
+     m = a.indexOf('--discard-prev');
+     if (m === 0) {
+      number = 'delete';
+     return number;
+    }           number = 'delete2';
+        return number;
+   }
+  return number;
+   })
+ for (let i = 0; i < k.length; i++) {
+   if (k[i] === 'delete2') {
+     k[i]= 'delete';
+     k[i-1] = 'delete';
+   }
+ }
+ function elete(num) {
+     return num !== 'delete';
+   }
+   return k.filter(elete);
+  };
